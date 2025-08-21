a1dd5b7e553125f45a94cc236e9a4c0a474ec83a
Justin <justinlillico@gmail.com>
2025-08-21 07:31:43 +0100

    Added a readme

---
+++ README.md
@@ -0,0 +1,6 @@
+
+You can interact with this app by going to the IP it displays in the top right.
+
+X.X.X.Whatever it displays:8080.
+
+/home/pi/.config/autostart/gym.desktop <-- This is where the autostart lives.
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from subprocess import call
+import socket
 
 app = Flask(__name__)
 
@@ -13,6 +14,31 @@ options.add_experimental_option('useAutomationExtension', False)
 
 driver = webdriver.Chrome(options=options)
 
+def get_identifier():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't need to be reachable, just used to select the right adapter
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip.split(".")[-1]
+
+
+watermark_code = f"""
+    const box = document.createElement('div');
+    box.innerText = '{get_identifier()}';
+    box.style.position = 'fixed';
+    box.style.top = '10px';      // use bottom: '10px' for bottom corner
+    box.style.right = '10px';    // change to left for left corner
+    box.style.background = 'black';
+    box.style.color = 'white';
+    box.style.padding = '5px 10px';
+    box.style.zIndex = '9999';
+    document.body.appendChild(box);
+"""
+
+driver.execute_script(watermark_code)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
